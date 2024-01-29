5fd53aedc499020ef39e2372e0df5320d859b300
SarayAnta <s.rodriguezanta@gmail.com>
2024-01-29 14:53:03 +0100

    create put method

--- src/service.js
+++ src/service.js
@@ -4,7 +4,7 @@
     const dataMovies = await moviesList.json();
     return dataMovies;
   }
- let moviesSectionTag = document.getElementById ("movies-list")
+ let moviesSectionTag = document.getElementById("movies-list")
  async function printMovies(){
     let movies = await getMovies();
     movies.map (movie => {
@@ -12,37 +12,64 @@
         `<div class="movie-item"><h3>${movie.name}</h3> 
         <p>${movie.gender}</p>
         <p><small>${movie.year}</small></p>
-        <button type="button" onclick="deleteMovie(${movie.id})">Delete</button>
-        <button type="button" onclick="editMovie"('${movie.id}')">Edit</button>
+        <button type="button" onclick="deleteMovie('${movie.id}')">Delete</button>
+        <button type="button" onclick="editMovie('${movie.id}')">Edit</button>
         <hr></div>`
     })
  }
  //DELETE
 async function deleteMovie (id){
-    const result = await fetch (`http://localhost:3000/movies/${id}`,
+    const result = await fetch(`http://localhost:3000/movies/${id}`,
     {method: "Delete"})
     return result
 }
 
-function countMovies() { //función para contar el nº de pelis y añadirlas en la función de crear
-    return document.querySelectorAll(".movie-item").length;
+//Método PUT U (update) del CRUD
+
+async function editMovie(id) {
+    const newTitle = prompt('Introduzca el título:');
+    const newGender = prompt('Introduzca el género:');
+    const newYear = prompt('Introduzca el año:');
+
+
+    const response = await fetch(`http://localhost:3000/movies/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: newTitle,
+            gender: newGender,
+            year: newYear
+        }),
+    });
+
+    return response
 }
+
+// function countMovies() { //función para contar el nº de pelis y añadirlas en la función de crear
+//     return document.querySelectorAll(".movie-item").length;
+// }
 
  //Método POST C (create) del CRUD
 async function createMovie() { 
-    const totalMovies = countMovies() + 1;
+    // const totalMovies = countMovies() + 1;
     const newMovie = {
-        "id": `${totalMovies}`, //la `` mete variables al string, la $ significa que es una variable de JS
+        // "id": `${totalMovies}`,
         "name": document.getElementById("tittle").value,
         "gender": document.getElementById("gender").value,
         "year": document.getElementById ("year").value
       }
     const result = await fetch (`http://localhost:3000/movies/`,
     { 
-        method: "POST", 
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
         body: JSON.stringify(newMovie)
     }
     )
 }
+
 printMovies ();
 
